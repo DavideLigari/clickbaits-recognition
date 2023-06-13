dc9b5b3feb8d2ee1fe6f5dc41accb901dc216d46
DavideLigari01 <davide.ligari01@universitadipavia.it>
2023-06-13 12:48:33 +0200

    analysing logistic regression

--- code/logistic_regression.py
+++ code/logistic_regression.py
@@ -1,39 +1,49 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 
 def logreg_inference(X, w, b):
+    """Return the probability of being positive."""
     z = X @ w + b
     p = 1 / (1 + np.exp(-z))
     return p
 
 
 def cross_entropy(P, Y):
+    """Return the cross-entropy loss."""
     P = np.clip(P, 0.0001, 0.9999)
     return (-Y * np.log(P) - (1 - Y) * np.log(1-P)).mean()
 
 
-def logreg_train(X, Y, X_valid, Y_valid, steps, lr, lambda_):
+def logreg_train(X, Y, X_valid, Y_valid, steps=10000, lr=0.01, batch_size=2000):
+    """Train a logistic regression classifier."""
+    validation_accuracy = []
+    train_accuracy = []
     m, n = X.shape
     w = np.zeros(n)
     b = 0
-    accs_train = []
-    accs_valid = []
     for step in range(steps+1):
-        P = logreg_inference(X, w, b)
-        if step % 1000 == 0:
-            P_valid = logreg_inference(X_valid, w, b)
-            prediction = (P_valid > 0.5)
-            accuracy = (prediction == Y_valid).mean()
-            accs_valid.append(accuracy*100)
-            print(
-                f"step {step}, accuracy {accuracy:.4f}")
-            P_train = logreg_inference(X, w, b)
-            prediction = (P_train > 0.5)
-            accuracy = (prediction == Y).mean()
-            accs_train.append(accuracy*100)
-        grad_w = ((P - Y) @ X) + 2 * lambda_ * w
-        grad_b = (P - Y).mean()
+        index = np.random.choice(X.shape[0], batch_size, replace=False)
+        newX = X[index]
+        newY = Y[index]
+
+        P = logreg_inference(newX, w, b)
+        grad_w = ((P - newY) @ newX) / batch_size
+        grad_b = (P - newY).mean()
+
         w -= lr * grad_w
         b -= lr * grad_b
-    return w, b, accs_valid, accs_train
+
+        if step % 1000 == 0:
+            P = logreg_inference(X, w, b)
+            predictions_train = (P > 0.5).astype(int)
+            accuracy_train = (predictions_train == Y).mean()*100
+            train_accuracy.append(accuracy_train)
+
+            P = logreg_inference(X_valid, w, b)
+            predictions_valid = (P > 0.5).astype(int)
+            accuracy_valid = (predictions_valid == Y_valid).mean()*100
+            validation_accuracy.append(accuracy_valid)
+            print(
+                f"Step {step}, train accuracy: {accuracy_train:.2f}%, validation accuracy: {accuracy_valid:.2f}%")
+
+    return w, b, train_accuracy, validation_accuracy
